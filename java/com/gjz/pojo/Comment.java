bbbfd33b14f6fd75ed5f382c7cd51c4fb9a7c0dc
GJZ <gjz0117@163.com>
2022-12-18 19:54:06 +0800

    set thymeleaf templates, develop five pojo by using jpa

---
+++ java/com/gjz/pojo/Comment.java
@@ -0,0 +1,34 @@
+package com.gjz.pojo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "t_comment")
+public class Comment {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String nickname;
+    private String email;
+    private String content;
+    private String avatar;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createTime;
+
+    @ManyToOne
+    private Blog blog;
+
+    @OneToMany(mappedBy = "parentComment")
+    private List<Comment> replyComment = new ArrayList<>();
+
+    @ManyToOne
+    private Comment parentComment;
+}
